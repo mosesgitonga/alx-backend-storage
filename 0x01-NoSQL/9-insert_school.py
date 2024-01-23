6cf8fa0ab7ee10f00c9141eb1ae0e0b7f41a939c
mosesgitonga <infosec947@gmail.com>
2024-01-23 19:32:54 +0300

    checker err

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,6 +6,9 @@ returns id of new created school
 
 
 def insert_school(mongo_collection, **kwargs):
+    """
+    inserting school
+    """
     res = mongo_collection.insert_one(kwargs)
     doc_id = res.inserted_id
 
--- 0x01-NoSQL/9-main.py
+++ 0x01-NoSQL/9-main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.10
+#!/usr/bin/env python3
 """ 9-main """
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
