11eefdde7f4ca335d88859bcbdc3765a75f1648b
mosesgitonga <infosec947@gmail.com>
2024-01-23 10:29:10 +0300

    listing all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+list docs
+"""
 from pymongo.collection import Collection
 from typing import List
 
@@ -8,10 +11,12 @@ def list_all(mongo_collection: Collection) -> List[dict]:
     Lists all documents in a MongoDB collection.
 
     Args:
-        mongo_collection (pymongo.collection.Collection): The MongoDB collection.
+        mongo_collection (pymongo.collection.Collection):
+         The MongoDB collection.
 
     Returns:
-        List[dict]: A list of dictionaries representing the documents in the collection.
+        List[dict]: A list of dictionaries representing
+        the documents in the collection.
     """
     cursor = mongo_collection.find({})
     documents = list(cursor)
