e918c6e305cfafdd664392df9c3eb9841b2ff68b
mosesgitonga <infosec947@gmail.com>
2024-01-24 16:05:57 +0300

    reading from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ creating a simple cache
 """
 import redis
 import uuid
-from typing import List, Union
+from typing import List, Union, Callable, Optional
 
 
 class Cache:
@@ -12,6 +12,9 @@ class Cache:
     writing strings to redis
     """
     def __init__(self):
+        """
+        instatiate
+        """
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
@@ -19,3 +22,15 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, int, bytes]]] = None) -> Union[str, int, bytes, None]:
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        return self.get(key, fn=int)
