ee97bf25e89bdcc69302043dac1f38ba5042ed50
mosesgitonga <infosec947@gmail.com>
2024-01-24 13:59:15 +0300

    specify db

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
     writing strings to redis
     """
     def __init__(self):
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
     def store(self, data: Union[str, int, bytes, float]) -> str:
